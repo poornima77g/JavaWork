de6d39351c2774cf511f0e5891971a2a8c414c97
unknown <poornima771993@gmail.com>
2019-11-05 15:31:07 +0530

    JavaFundamentals added using Eclipse

---
+++ src/ForLoop1.java
@@ -0,0 +1,12 @@
+//package JavaFundamentals
+
+public class ForLoop1 {
+	public static void main(String[] args)
+	{
+		for(int i=0; i<3; i++) {
+			System.out.println("Hello Java...." +i);
+		System.out.println("I have done......!");
+	}
+
+}
+}
---
+++ src/HelloWorld.java
@@ -0,0 +1,13 @@
+//package JavaFundamentals;
+
+public class HelloWorld{
+
+	public static void main(String[] args)
+	{
+		for(int i=0; i<3; i++)
+		System.out.println("Hello Java..." +i);
+		
+	}
+
+
+}
